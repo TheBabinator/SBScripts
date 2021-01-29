432d4a7445ee797f599fbbb53b758c4a8573b6f3
TheBabinator <Sebastiaan.Boom@outlook.com>
2021-01-29 18:24:39 +0000

    HHHHHHHHHHHHHHHHHH

--- patapon/localRythm.lua
+++ patapon/localRythm.lua
@@ -9,6 +9,8 @@ local floor = math.floor
 local round = math.round
 local deg = math.deg
 local atan = math.atan
+local color = Color3.new
+local ud2 = UDim2.new
 
 local function instance(name, properties)
     local new = Instance.new(name)
@@ -76,8 +78,8 @@ local worm = {
     frame = instance("Frame", {
         BackgroundTransparency = 1;
         AnchorPoint = Vector2.new(0, 1);
-        Position = UDim2.new(0, -500, 0.4, 0);
-        Size = UDim2.new(0, 300, 0, 45);
+        Position = ud2(0, -500, 0.4, 0);
+        Size = ud2(0, 300, 0, 45);
         Parent = GUI;
     });
     length = 400;
@@ -94,14 +96,14 @@ worm.text = instance("TextLabel", {
     ZIndex = 11;
     Text = "0";
     TextSize = 64;
-    TextColor3 = Color3.new(1, 0, 0);
+    TextColor3 = color(1, 0, 0);
     TextStrokeTransparency = 0;
     BackgroundTransparency = 1;
     TextXAlignment = Enum.TextXAlignment.Right;
     TextYAlignment = Enum.TextYAlignment.Bottom;
     Font = Enum.Font.FredokaOne;
-    Position = UDim2.new(0, 0, 0.4, 20);
-    Size = UDim2.new(0, 100, 0, 0);
+    Position = ud2(0, 0, 0.4, 20);
+    Size = ud2(0, 100, 0, 0);
     Parent = GUI;
 })
 
@@ -109,14 +111,14 @@ worm.subtext = instance("TextLabel", {
     ZIndex = 11;
     Text = "COMBO!";
     TextSize = 24;
-    TextColor3 = Color3.new(1, 1, 1);
+    TextColor3 = color(1, 1, 1);
     TextStrokeTransparency = 0;
     BackgroundTransparency = 1;
     TextXAlignment = Enum.TextXAlignment.Left;
     TextYAlignment = Enum.TextYAlignment.Bottom;
     Font = Enum.Font.FredokaOne;
-    Position = UDim2.new(1, 6, 0, 0);
-    Size = UDim2.new(1, 0, 1, 0);
+    Position = ud2(1, 6, 0, 0);
+    Size = ud2(1, 0, 1, 0);
     Parent = worm.text;
 })
 
@@ -124,17 +126,17 @@ for x = 0, worm.length - 1 do
     local new = instance("Frame", {
         ZIndex = 10;
         BorderSizePixel = 0;
-        BackgroundColor3 = Color3.new(0.1, 0.1, 0.1);
-        Position = UDim2.new(0, x, 0, 0);
-        Size = UDim2.new(0, 1, 1, 0);
+        BackgroundColor3 = color(0.1, 0.1, 0.1);
+        Position = ud2(0, x, 0, 0);
+        Size = ud2(0, 1, 1, 0);
         Parent = worm.frame;
     })
     local newBack = instance("Frame", {
         ZIndex = 8;
         BorderSizePixel = 0;
-        BackgroundColor3 = Color3.new(0.1, 0.1, 0.1);
-        Position = UDim2.new(0, 0, 0, -5);
-        Size = UDim2.new(1, 0, 1, 10);
+        BackgroundColor3 = color(0.1, 0.1, 0.1);
+        Position = ud2(0, 0, 0, -5);
+        Size = ud2(1, 0, 1, 10);
         Parent = new;
     })
     worm.segments[x] = new
@@ -146,8 +148,8 @@ worm.head = instance("ImageLabel", {
     Image = "rbxassetid://6092002256";
     BackgroundTransparency = 1;
     AnchorPoint = Vector2.new(0.5, 0);
-    Position = UDim2.new(0, 0, 0, 0);
-    Size = UDim2.new(0, 150, 1, 0);
+    Position = ud2(0, 0, 0, 0);
+    Size = ud2(0, 150, 1, 0);
     Parent = worm.segments[worm.length - 1];
 })
 
@@ -155,8 +157,8 @@ worm.back = instance("ImageLabel", {
     ZIndex = 8;
     Image = "rbxassetid://6092002256";
     BackgroundTransparency = 1;
-    Position = UDim2.new(0, -5, 0, -5);
-    Size = UDim2.new(1, 10, 1, 10);
+    Position = ud2(0, -5, 0, -5);
+    Size = ud2(1, 10, 1, 10);
     Parent = worm.head;
 })
 
@@ -165,8 +167,8 @@ worm.top = instance("ImageLabel", {
     Image = "rbxassetid://6092325605";
     BackgroundTransparency = 1;
     AnchorPoint = Vector2.new(1, 1);
-    Position = UDim2.new(1, -20, 0, 5);
-    Size = UDim2.new(0, 50, 0, 25);
+    Position = ud2(1, -20, 0, 5);
+    Size = ud2(0, 50, 0, 25);
     Parent = worm.head;
 })
 
@@ -174,25 +176,25 @@ worm.eyes = instance("ImageLabel", {
     ZIndex = 9;
     Image = "rbxassetid://6092325147";
     BackgroundTransparency = 1;
-    Position = UDim2.new(0, 0, 0, 0);
-    Size = UDim2.new(1, 0, 1, 0);
+    Position = ud2(0, 0, 0, 0);
+    Size = ud2(1, 0, 1, 0);
     Parent = worm.top;
 })
 
-local guiBeatFrame = instance("Frame", { BackgroundTransparency = 1; AnchorPoint = Vector2.new(0.5, 0.5); Position = UDim2.new(0.5, 0, 0.5, 0); Size = UDim2.new(1, -32, 1, -32); Parent = GUI; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(0, 0, 0, 0); Size = UDim2.new(1, 0, 0, 8); Parent = guiBeatFrame; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(0, 0, 1, -8); Size = UDim2.new(1, 0, 0, 8); Parent = guiBeatFrame; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(0, 0, 0, 8); Size = UDim2.new(0, 8, 1, -16); Parent = guiBeatFrame; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(1, -8, 0, 8); Size = UDim2.new(0, 8, 1, -16); Parent = guiBeatFrame; })
-local guiBeatFrameOutlines = instance("Frame", { BackgroundTransparency = 1; AnchorPoint = Vector2.new(0.5, 0.5); Position = UDim2.new(0.5, 0, 0.5, 0); Size = UDim2.new(1, -32, 1, -32); Parent = GUI; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(0, 0, 0, 0); Size = UDim2.new(1, 0, 0, 3); Parent = guiBeatFrameOutlines; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(0, 0, 1, -3); Size = UDim2.new(1, 0, 0, 3); Parent = guiBeatFrameOutlines; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(0, 0, 0, 3); Size = UDim2.new(0, 3, 1, -6); Parent = guiBeatFrameOutlines; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(1, -3, 0, 3); Size = UDim2.new(0, 3, 1, -6); Parent = guiBeatFrameOutlines; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(0, 5, 0, 5); Size = UDim2.new(1, -10, 0, 3); Parent = guiBeatFrameOutlines; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(0, 5, 1, -8); Size = UDim2.new(1, -10, 0, 3); Parent = guiBeatFrameOutlines; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(0, 5, 0, 8); Size = UDim2.new(0, 3, 1, -16); Parent = guiBeatFrameOutlines; })
-instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = UDim2.new(1, -8, 0, 8); Size = UDim2.new(0, 3, 1, -16); Parent = guiBeatFrameOutlines; })
+local guiBeatFrame = instance("Frame", { BackgroundTransparency = 1; AnchorPoint = Vector2.new(0.5, 0.5); Position = ud2(0.5, 0, 0.5, 0); Size = ud2(1, -32, 1, -32); Parent = GUI; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(0, 0, 0, 0); Size = ud2(1, 0, 0, 8); Parent = guiBeatFrame; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(0, 0, 1, -8); Size = ud2(1, 0, 0, 8); Parent = guiBeatFrame; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(0, 0, 0, 8); Size = ud2(0, 8, 1, -16); Parent = guiBeatFrame; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(1, -8, 0, 8); Size = ud2(0, 8, 1, -16); Parent = guiBeatFrame; })
+local guiBeatFrameOutlines = instance("Frame", { BackgroundTransparency = 1; AnchorPoint = Vector2.new(0.5, 0.5); Position = ud2(0.5, 0, 0.5, 0); Size = ud2(1, -32, 1, -32); Parent = GUI; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(0, 0, 0, 0); Size = ud2(1, 0, 0, 3); Parent = guiBeatFrameOutlines; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(0, 0, 1, -3); Size = ud2(1, 0, 0, 3); Parent = guiBeatFrameOutlines; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(0, 0, 0, 3); Size = ud2(0, 3, 1, -6); Parent = guiBeatFrameOutlines; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(1, -3, 0, 3); Size = ud2(0, 3, 1, -6); Parent = guiBeatFrameOutlines; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(0, 5, 0, 5); Size = ud2(1, -10, 0, 3); Parent = guiBeatFrameOutlines; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(0, 5, 1, -8); Size = ud2(1, -10, 0, 3); Parent = guiBeatFrameOutlines; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(0, 5, 0, 8); Size = ud2(0, 3, 1, -16); Parent = guiBeatFrameOutlines; })
+instance("Frame", { ZIndex = 30; BorderSizePixel = 0; Position = ud2(1, -8, 0, 8); Size = ud2(0, 3, 1, -16); Parent = guiBeatFrameOutlines; })
 
 RunService.RenderStepped:Connect(function(delta)
     frame = frame + 1
@@ -258,7 +260,7 @@ RunService.RenderStepped:Connect(function(delta)
     if rythm.fever == 0 then
         batch(guiBeatFrame, {
             BackgroundTransparency = rythm.rawBeat % 1;
-            BackgroundColor3 = Color3.new(1, 1, 1);
+            BackgroundColor3 = color(1, 1, 1);
         })
         batch(guiBeatFrameOutlines, {
             BackgroundTransparency = 1;
@@ -268,28 +270,28 @@ RunService.RenderStepped:Connect(function(delta)
             if rythm.fever < 1 then
                 batch(guiBeatFrame, {
                     BackgroundTransparency = rythm.rawBeat % 1;
-                    BackgroundColor3 = Color3.new(1, 1, 1);
+                    BackgroundColor3 = color(1, 1, 1);
                 })
             else
                 if frame % 8 >= 6 then
                     batch(guiBeatFrame, {
                         BackgroundTransparency = rythm.rawBeat % 1;
-                        BackgroundColor3 = Color3.new(1, 1, 1)
+                        BackgroundColor3 = color(1, 1, 1)
                     })
                 elseif frame % 8 >= 4 then
                     batch(guiBeatFrame, {
                         BackgroundTransparency = rythm.rawBeat % 1;
-                        BackgroundColor3 = Color3.new(0.2, 1, 1)
+                        BackgroundColor3 = color(0.2, 1, 1)
                     })
                 elseif frame % 6 >= 2 then
                     batch(guiBeatFrame, {
                         BackgroundTransparency = rythm.rawBeat % 1;
-                        BackgroundColor3 = Color3.new(1, 1, 0.2)
+                        BackgroundColor3 = color(1, 1, 0.2)
                     })
                 else
                     batch(guiBeatFrame, {
                         BackgroundTransparency = rythm.rawBeat % 1;
-                        BackgroundColor3 = Color3.new(0.2, 1, 0.2);
+                        BackgroundColor3 = color(0.2, 1, 0.2);
                     })
                 end
             end
@@ -301,18 +303,18 @@ RunService.RenderStepped:Connect(function(delta)
                 if frame % 4 < 2 then
                     batch(guiBeatFrameOutlines, {
                         BackgroundTransparency = rythm.rawBeat % 1;
-                        BackgroundColor3 = Color3.new(0.2, 0.2, 0.2);
+                        BackgroundColor3 = color(0.2, 0.2, 0.2);
                     })
                 else
                     batch(guiBeatFrameOutlines, {
                         BackgroundTransparency = rythm.rawBeat % 1;
-                        BackgroundColor3 = Color3.new(1, 1, 1);
+                        BackgroundColor3 = color(1, 1, 1);
                     })
                 end
             else
                 batch(guiBeatFrameOutlines, {
                     BackgroundTransparency = rythm.rawBeat % 1;
-                    BackgroundColor3 = Color3.new(0.2, 0.2, 0.2);
+                    BackgroundColor3 = color(0.2, 0.2, 0.2);
                 })
             end
             batch(guiBeatFrame, {
@@ -325,7 +327,7 @@ RunService.RenderStepped:Connect(function(delta)
         if worm.visible then
             worm.visible = false
             worm.text.Visible = false
-            worm.frame.Position = UDim2.new(0, lerp(worm.frame.Position.X.Offset, -worm.length, 0.01), 0.4, 0)
+            worm.frame.Position = ud2(0, lerp(worm.frame.Position.X.Offset, -worm.length, 0.01), 0.4, 0)
         end
     else
         if not worm.visible then
@@ -335,45 +337,45 @@ RunService.RenderStepped:Connect(function(delta)
         if rythm.fever < 0.5 then
             worm.text.Text = rythm.combo
             worm.subtext.Text = "COMBO!"
-            worm.subtext.TextColor3 = Color3.new(1, 1, 1)
+            worm.subtext.TextColor3 = color(1, 1, 1)
             worm.subtext.TextXAlignment = Enum.TextXAlignment.Left
             worm.subtext.TextSize = 24
-            worm.head.ImageColor3 = Color3.new(0.1, 0.1, 0.1)
+            worm.head.ImageColor3 = color(0.1, 0.1, 0.1)
             worm.top.ImageColor3 = worm.head.ImageColor3
             worm.back.ImageColor3 = worm.head.ImageColor3
-            worm.frame.Position = UDim2.new(0, lerp(worm.frame.Position.X.Offset, -worm.length + exp(rythm.fever, 0.01) * 250, 0.1), 0.4, 0)
+            worm.frame.Position = ud2(0, lerp(worm.frame.Position.X.Offset, -worm.length + exp(rythm.fever, 0.01) * 250, 0.1), 0.4, 0)
             worm.scale.Scale = 1
             for x, segment in pairs(worm.segments) do
                 local y1 = -lerp(0, sin(x / worm.length * 6 * tau) * 5 + 5, abs(sin(rythm.rawBeat * 0.5 * tau)))
                 local y2 = 0
                 local y = lerp(y1, y2, exp(x / worm.length, 1000))
-                segment.Position = UDim2.new(0, x, 0, y)
-                segment.BackgroundColor3 = Color3.new(0.1, 0.1, 0.1)
+                segment.Position = ud2(0, x, 0, y)
+                segment.BackgroundColor3 = color(0.1, 0.1, 0.1)
             end
             for x, segment in pairs(worm.segmentsBack) do
-                segment.BackgroundColor3 = Color3.new(0.1, 0.1, 0.1)
+                segment.BackgroundColor3 = color(0.1, 0.1, 0.1)
             end
             worm.head.Rotation = 0
         elseif rythm.fever < 1 then
             worm.text.Text = rythm.combo
             worm.subtext.Text = "COMBO!"
-            worm.subtext.TextColor3 = Color3.new(1, 1, 1)
+            worm.subtext.TextColor3 = color(1, 1, 1)
             worm.subtext.TextXAlignment = Enum.TextXAlignment.Left
             worm.subtext.TextSize = 24
-            worm.head.ImageColor3 = Color3.new(0.1, 0.1, 0.1)
+            worm.head.ImageColor3 = color(0.1, 0.1, 0.1)
             worm.top.ImageColor3 = worm.head.ImageColor3
             worm.back.ImageColor3 = worm.head.ImageColor3
-            worm.frame.Position = UDim2.new(0, lerp(worm.frame.Position.X.Offset, -worm.length + exp(rythm.fever, 0.01) * 300, 0.1), 0.4, 0)
+            worm.frame.Position = ud2(0, lerp(worm.frame.Position.X.Offset, -worm.length + exp(rythm.fever, 0.01) * 300, 0.1), 0.4, 0)
             worm.scale.Scale = 1
             for x, segment in pairs(worm.segments) do
                 local y1 = -lerp(0, sin(x / worm.length * 6 * tau) * 5 + 5, abs(sin(rythm.rawBeat * 0.5 * tau)))
                 local y2 = sin((rythm.rawBeat + x / worm.length) * tau) * 40
                 local y = lerp(y1, y2, exp(x / worm.length, 400))
-                segment.Position = UDim2.new(0, x, 0, y)
-                segment.BackgroundColor3 = Color3.new(0.1, 0.1, 0.1)
+                segment.Position = ud2(0, x, 0, y)
+                segment.BackgroundColor3 = color(0.1, 0.1, 0.1)
             end
             for x, segment in pairs(worm.segmentsBack) do
-                segment.BackgroundColor3 = Color3.new(0.1, 0.1, 0.1)
+                segment.BackgroundColor3 = color(0.1, 0.1, 0.1)
             end
             local gradient = grad(1, function(x)
                 local y = sin((rythm.rawBeat + x / worm.length) * tau) * 40
@@ -384,23 +386,23 @@ RunService.RenderStepped:Connect(function(delta)
         else
             worm.text.Text = ""
             worm.subtext.Text = "FEVER!"
-            worm.subtext.TextColor3 = Color3.new(1, 0.6, 0):Lerp(Color3.new(1, 0.3, 0), rythm.rawBeat % 1)
+            worm.subtext.TextColor3 = color(1, 0.6, 0):Lerp(color(1, 0.3, 0), rythm.rawBeat % 1)
             worm.subtext.TextXAlignment = Enum.TextXAlignment.Center
             worm.subtext.TextSize = 64
-            worm.head.ImageColor3 = rythm.fever >= 2 and Color3.new(1, 0.8, 0.1):Lerp(Color3.new(1, 0.4, 0.1), rythm.rawBeat % 1) or Color3.new(1, 0.25, 0.1):Lerp(Color3.new(1, 0.1, 0.1), rythm.rawBeat % 1)
+            worm.head.ImageColor3 = rythm.fever >= 2 and color(1, 0.8, 0.1):Lerp(color(1, 0.4, 0.1), rythm.rawBeat % 1) or color(1, 0.25, 0.1):Lerp(color(1, 0.1, 0.1), rythm.rawBeat % 1)
             worm.top.ImageColor3 = worm.head.ImageColor3
-            worm.back.ImageColor3 = Color3.new(1, 1, 1)
-            worm.frame.Position = UDim2.new(0, lerp(worm.frame.Position.X.Offset, -250, 0.01), 0.4, 0)
+            worm.back.ImageColor3 = color(1, 1, 1)
+            worm.frame.Position = ud2(0, lerp(worm.frame.Position.X.Offset, -250, 0.01), 0.4, 0)
             worm.scale.Scale = 1 + (1 - (rythm.rawBeat % 1)) * 0.25
             for x, segment in pairs(worm.segments) do
                 local y1 = sin((x / worm.length - rythm.rawBeat) * tau) * 20
                 local y2 = 0
                 local y = lerp(y1, y2, exp(x / worm.length, 1000))
-                segment.Position = UDim2.new(0, x, 0, y)
-                segment.BackgroundColor3 = rythm.fever - 1 >= (x / worm.length) and Color3.new(1, 0.8, 0.1):Lerp(Color3.new(1, 0.4, 0.1), rythm.rawBeat % 1) or Color3.new(1, 0.25, 0.1):Lerp(Color3.new(1, 0.1, 0.1), rythm.rawBeat % 1)
+                segment.Position = ud2(0, x, 0, y)
+                segment.BackgroundColor3 = rythm.fever - 1 >= (x / worm.length) and color(1, 0.8, 0.1):Lerp(color(1, 0.4, 0.1), rythm.rawBeat % 1) or color(1, 0.25, 0.1):Lerp(color(1, 0.1, 0.1), rythm.rawBeat % 1)
             end
             for x, segment in pairs(worm.segmentsBack) do
-                segment.BackgroundColor3 = Color3.new(1, 1, 1)
+                segment.BackgroundColor3 = color(1, 1, 1)
             end
             worm.head.Rotation = 0
         end
